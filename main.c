70c0053cbd31a0a069c838b7b19dd1c3c5bf2bc0
Samu <Samu.kirjonen@gmail.com>
2025-04-24 14:45:36 +0300

    palautus

--- main.c
+++ main.c
@@ -92,7 +92,7 @@ int main() {
             write_status_to_eeprom(data);
         }
 
-        // Tällä hetkellä vaatii lora yhteyden kalibroinnin aloittamiseen
+        //Tällä hetkellä vaatii lora yhteyden kalibroinnin aloittamiseen
         switch (data.state) {
             case BOOT:
                 printf("Connecting to Lora...\n");
