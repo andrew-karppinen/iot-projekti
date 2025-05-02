caab3ab130ca891c82f6a6eef0921606408e247e
ak <awkk12345@gmail.com>
2025-05-02 14:39:56 +0300

    Paranneltu pyöritystä ja virrankatkaisun tunnistusta

--- main.c
+++ main.c
@@ -145,7 +145,6 @@ int main() {
 
                 // moottori
                 if (run_motor_30(&data)) {
-                    data.pill_counter++;
 
                     write_status_to_eeprom(data);
 
